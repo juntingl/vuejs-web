25a943500f97fa87d88cdcb2c2e61f6d15ad81aa
twitch.liu <twitch.liu@huolala.cn>
2018-10-10 13:53:49 +0800

    static file  path modify

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,9 @@
 module.exports = {
-  outputDir: '/vuejs-web/dist/'
+  outputDir: './vuejs-web/dist/',
+  baseUrl: undefined,
+  assetsDir: './vuejs-web/dist/',
+  runtimeCompiler: undefined,
+  productionSourceMap: undefined,
+  parallel: undefined,
+  css: undefined
 }

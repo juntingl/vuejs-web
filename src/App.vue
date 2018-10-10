8b0882412e1e7bf3c186e15b74edb9fdd2669bd2
twitch.liu <twitch.liu@huolala.cn>
2018-10-10 14:47:00 +0800

    引入 bootstrap-sass、font-awesome UI库和字体库

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,31 @@
 <template>
-  <div id="app">
-    <img alt="Vue Web" src="./assets/logo.png">
-    <h1>Welcome to Vuejs BBS!</h1>
+  <div class="container">
+    <div class="jumbotron">
+      <div class="container">
+        <h1>Welcome from bootstrap</h1>
+        <p>This is a simple hero unit, a simple jumbotron-style component for calling extra attention to featured content or information.</p>
+        <p>
+          <a class="btn btn-primary btn-lg" href="http://www.bootcss.com/" role="button" target="_blank">
+            Learn more
+            <span class="glyphicon glyphicon-hand-right" aria-hidden="true"></span>
+          </a>
+        </p>
+      </div>
+    </div>
+    <div class="jumbotron">
+      <div class="container">
+        <h1>Welcome from Font Awesome</h1>
+        <p>Get vector icons and social logos on your website with Font Awesome, the web’s most popular icon set and toolkit.</p>
+        <p>
+          <i class="fa fa-camera"></i>
+          <i class="fa fa-camera fa-2x"></i>
+          <i class="fa fa-camera fa-3x"></i>
+          <i class="fa fa-camera fa-4x"></i>
+          <i class="fa fa-camera fa-5x"></i>
+          <i class="fa fa-spinner fa-spin"></i>
+        </p>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -12,15 +36,11 @@ export default {
 </script>
 
 <style lang="scss">
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-h1 {
-  color: #42b983;
-}
+// 波浪号（~）会将后面的内容当模块路径对待，默认指向 node_modules 目录。
+$icon-font-path: "~bootstrap-sass/assets/fonts/bootstrap/";
+$fa-font-path: "~font-awesome/fonts/";
+@import "~bootstrap-sass/assets/stylesheets/_bootstrap";
+@import "~font-awesome/scss/font-awesome";
+
+body > .container { margin-top: 15px; }
 </style>

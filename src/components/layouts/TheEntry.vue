70240e5952567f9814f028e5f32771e2ac2c3436
Junting <342766475@qq.com>
2018-10-22 00:24:48 +0800

    增加导航栏入口组件

---
+++ src/components/layouts/TheEntry.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="navbar-right">
+    <div class="nav navbar-nav github-login">
+      <a href="#" class="btn btn-default login-btn">
+        <i class="fa fa-user"></i> 登 录
+      </a>
+      <a href="#" class="btn btn-default login-btn">
+        <i class="fa fa-user-plus"></i> 注 册
+      </a>
+    </div>
+  </div>·
+</template>
+
+<script>
+export default {
+  name: 'TheEntry'
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
--- src/components/layouts/TheHeader.vue
+++ src/components/layouts/TheHeader.vue
@@ -26,14 +26,23 @@
             <a href="#" @click="handleNavClick(index)">{{ item }}</a>
           </li>
         </ul>
+        <!-- 入口组件 -->
+        <div class="navbar-right">
+          <TheEntry/>
+        </div>
       </div>
     </div>
   </div>
 </template>
 
 <script>
+import TheEntry from '@/components/layouts/TheEntry';
+
 export default {
   name: 'TheHeader',
+  components: {
+    TheEntry
+  },
   data() {
     return {
       logo: {

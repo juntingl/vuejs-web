bb7fce5dbc6b64483bc87b17723828d770fd7cbd
Junting <342766475@qq.com>
2018-11-05 22:44:12 +0800

    引入 sweetalert2 弹窗插件，并进行封装到 Vue 中

--- src/plugins/vue-sweetalert2.js
+++ src/plugins/vue-sweetalert2.js
@@ -5,12 +5,13 @@
  * @email: 342766475@qq.com
  * @Date: 2018-11-05 22:36:27
  * @Last Modified by: pycoder.Junting
- * @Last Modified time: 2018-11-05 22:40:45
+ * @Last Modified time: 2018-11-05 22:42:31
  */
 
  import swal from 'sweetalert2';
 
  export default {
+   // Vue 插件扩展公开方法
    install: (Vue) => {
     //  sweetalert2 设置，默认配置的方法
     swal.setDefaults({
